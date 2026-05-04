b2b6c4a64596884d8dee182d176166c5d552f5ff
AndCplusplus <acade013@jaguar.tamu.edu>
2026-05-04 14:20:45 -0500

    Update Home.py

--- src/frontview/Home.py
+++ src/frontview/Home.py
@@ -10,8 +10,7 @@ st.set_page_config(
 
 st.title("🌲 Trailguard 🛡️")
 st.markdown("---")
-st.markdown(<div style="background-image: url(https://github.com/collincan2/trail-guard/blob/main/src/PilotPhotos/treefall4.jpg?raw=true); height: 400; padding: 20;">
-</div>)
+
 
 st.markdown("""
 ### Welcome to the Trailguard Park Management Dashboard!
