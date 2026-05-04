11bd00fd57f7f4adc9e439333beeab8efb0903ab
AndCplusplus <acade013@jaguar.tamu.edu>
2026-05-04 14:22:53 -0500

    Update Home.py

--- src/frontview/Home.py
+++ src/frontview/Home.py
@@ -1,5 +1,28 @@
 # src/frontview/app.py
 import streamlit as st
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:src/PilotPhotos/blackbear18.jpg;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
 
 # Streamlit init
 st.set_page_config(
